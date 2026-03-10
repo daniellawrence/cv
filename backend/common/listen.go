be5b9abca852586429258acd1d0cd046f205f724
Daniel Lawrence <github@dansysadm.com>
2026-03-11 08:17:02 +1100

    feat(zpages): add healthz endpoint

--- backend/common/listen.go
+++ backend/common/listen.go
@@ -5,7 +5,24 @@ import (
 	"net/http"
 )
 
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+
+	_, err := w.Write([]byte("ok"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func zPages(mux *http.ServeMux) {
+	mux.HandleFunc("/healthz", healthz)
+}
+
 func Listen(mux *http.ServeMux) error {
+
+	zPages(mux)
+
 	addr := GetListenAddr()
 	log.Printf("Starting server on %s\n", addr)
 
