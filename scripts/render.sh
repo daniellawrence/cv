0bd8a830927131eced48356b309ff3dea8c82a2f
Daniel Lawrence <github@dansysadm.com>
2026-03-14 20:19:13 +1100

    feat(ghcr.io): use github built images in production

--- scripts/render.sh
+++ scripts/render.sh
@@ -2,14 +2,14 @@
 source $(dirname $0)/lib.sh
 cd $GIT_ROOT
 
-export ENVIRONMENT="dev"
-if [[ "${1}" == "prod" ]];then
-    export ENVIRONMENT="production"
-fi
 
-./bin/helmfile  \
-    --environment ${ENVIRONMENT} \
-    --helm-binary ${PWD}/bin/helm \
-    --file infra/deployments/helmfile.yaml \
-    template \
-    --output-dir-template "artifacts/${ENVIRONMENT}/{{ .Release.Name }}/" 
+for ENVIRONMENT in "dev" "production"; do
+
+    ./bin/helmfile  \
+        --environment ${ENVIRONMENT} \
+        --helm-binary ${PWD}/bin/helm \
+        --file infra/deployments/helmfile.yaml \
+        template \
+        --output-dir-template "artifacts/${ENVIRONMENT}/{{ .Release.Name }}/" 
+
+done
