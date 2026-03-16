f3ad41c36a8eb780de99d574dd1e53d7ff4794fc
Daniel Lawrence <github@dansysadm.com>
2026-03-17 10:32:38 +1100

    chore: update render script and regenerate manifests

--- scripts/render.sh
+++ scripts/render.sh
@@ -5,31 +5,14 @@ cd $GIT_ROOT
 
 for ENVIRONMENT in "dev" "production"; do
 
-    ./bin/helmfile  \
-        --environment ${ENVIRONMENT} \
-        --helm-binary ${PWD}/bin/helm \
-        --file infra/deployments/helmfile.yaml \
-        --selector tier=tracing \
-        template \
-        --output-dir-template "artifacts/${ENVIRONMENT}/{{ .Release.Name }}/"
+    # Clear output directory before rendering to avoid duplicates
+    rm -rf infra/deployments/artifacts/${ENVIRONMENT}/*
 
     ./bin/helmfile  \
         --environment ${ENVIRONMENT} \
         --helm-binary ${PWD}/bin/helm \
         --file infra/deployments/helmfile.yaml \
-        --selector tier=app \
         template \
         --output-dir-template "artifacts/${ENVIRONMENT}/{{ .Release.Name }}/"
-
-    if [[ "${ENVIRONMENT}" == "dev" ]];then
-
-        ./bin/helmfile \
-            --environment ${ENVIRONMENT} \
-            --helm-binary ${PWD}/bin/helm \
-            --file infra/deployments/helmfile.yaml \
-            --selector tier=deploy \
-            template \
-            --output-dir-template "artifacts/${ENVIRONMENT}/{{ .Release.Name }}/"
-        fi
 
 done
