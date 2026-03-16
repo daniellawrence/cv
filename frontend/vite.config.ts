e7b8bd20d8164281710314b73e9d6566b310af77
Daniel Lawrence <github@dansysadm.com>
2026-03-16 13:46:23 +1100

    bug(frontend):  “@bufbuild/protobuf/codegenv2” was a bare specifier, but was not remapped to anything

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "node:path"
 
 export default defineConfig({
     plugins: [react()],
-    build: {
-        rollupOptions: {
-            external: ['@bufbuild/protobuf/codegenv2']
+    resolve: {
+        alias: {
+            proto: path.resolve(__dirname, "../gen/ts")
+        },
+        preserveSymlinks: true,
+    },
+    optimizeDeps: {
+        include: [
+            "@bufbuild/protobuf",
+            "@bufbuild/protobuf/codegenv2",
+        ],
+    },
+    server: {
+        fs: {
+            allow: [".."]
         }
     }
 })
